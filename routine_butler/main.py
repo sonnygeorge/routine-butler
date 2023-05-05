a2b28c25a78dd9acdcb386c15c1e9497442ec63e
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-05-05 12:10:14 -0600

    Improved readability

--- routine_butler/main.py
+++ routine_butler/main.py
@@ -23,8 +23,8 @@ class MainApp:
             warning=CLR_CODES.warning,
         )
 
-        self.header = Header()
-        self.main_frame = ui.element("div")
+        self.header = Header()  # add header to UI
+        self.main_frame = ui.element("div")  # add empty div for app's content
 
         if auto_login is not None:
             self.handle_login_attempt(username=auto_login)
@@ -32,6 +32,7 @@ class MainApp:
             self.login_gui()
 
     def login_gui(self):
+        """GUI for user to specify their user"""
         with self.main_frame:
             with ui.card():
                 ui.label("Login")
@@ -45,23 +46,31 @@ class MainApp:
                 )
 
     def main_gui(self, user):
+        """The main GUI for the app"""
         routines_sidebar_ = routines_sidebar(self.engine, user)
         self.header.routines_button.on("click", routines_sidebar_.toggle)
 
     def handle_login_attempt(self, username: str):
+        """Retrieves or creates a user and then displays the main GUI
+
+        NOTE: this flow can be augmented to be more sophisticated later
+        """
+        # query db for user
         is_user_filter_expr = User.Config.orm_model.username == username
         user = User.query_one(self.engine, filter_=is_user_filter_expr)
 
         if user is None:
+            # create new user w/ username
             user = User(username=username)
             user.add_self_to_db(self.engine)
-            self.main_frame.clear()
-            ui.notify("Welcome, new user:" + username + "!")
-            self.main_gui(user)
+
+            welcome_msg = f"Welcome, new user: {username}!"
         else:
-            self.main_frame.clear()
-            ui.notify("Welcome back, " + username + "!")
-            self.main_gui(user)
+            welcome_msg = "Welcome back, " + username + "!"
+
+        self.main_frame.clear()
+        ui.notify(welcome_msg)
+        self.main_gui(user)
 
 
 def main(engine: Engine, auto_login: Optional[str] = None):
