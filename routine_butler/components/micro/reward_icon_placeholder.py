ab5ae1fbe6983bbc338cd3ae20a923f6e19519c8
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-05-30 09:40:43 -0500

    Fixed: layout tweaks, cli testing arg

--- routine_butler/components/micro/reward_icon_placeholder.py
+++ routine_butler/components/micro/reward_icon_placeholder.py
@@ -5,7 +5,10 @@ from routine_butler.constants import ABS_REWARD_SVG_PATH, SVG_SIZE
 
 
 def reward_icon_placeholder() -> ui.element:
-    placeholder = ui.element("q-item").props("dense")
+    placeholder = ui.element("q-item").props("dense").style("height: 2.5rem;")
+    cls = "items-center justify-center border-secondary"
+    cls += " rounded w-full border-dotted border-2"
+    placeholder.classes(cls)
     with placeholder:
         SVG(
             ABS_REWARD_SVG_PATH,
--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 import argparse
+from typing import Protocol
 
 from routine_butler.main import main
+
+
+class RunArgs(Protocol):
+    testing: bool
+
 
 if __name__ in {"__main__", "__mp_main__"}:
     parser = argparse.ArgumentParser(description="Runs RoutineButler")
     help = "If used, the app will run with a test database and auto-login w/ a test user"
     parser.add_argument("--testing", action="store_true", help=help)
-    args = parser.parse_args()
-    main(testing=True)
+    args: RunArgs = parser.parse_args()
+    main(testing=args.testing)
