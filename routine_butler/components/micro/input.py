5d3c95d33ef859eac8603ca8107eaeccf957c063
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-06-19 17:07:53 -0500

    Removed accidental buggy import

--- routine_butler/components/micro/input.py
+++ routine_butler/components/micro/input.py
@@ -2,8 +2,6 @@ import subprocess
 
 from nicegui import ui
 
-from routine_butler.utils import close_keyboard, open_keyboard
-
 # Don't forget to change the default keyboard to keyboard-lq1.xml
 # stackoverflow.com/questions/70574505/how-to-change-the-default-matchbox-keyboard-layout
 
