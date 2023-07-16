5ec3efcea4480ff7557c851ba1c27f1e966b5426
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-07-15 23:45:22 -0600

    Removed temporary debug logging

--- routine_butler/views/ring.py
+++ routine_butler/views/ring.py
@@ -28,7 +28,6 @@ async def async_play_wav_with_volume_adjustment(
 
 def ring_next_alarm():
     """Does the steps associated with "ringing" the next alarm of the global state"""
-    logger.debug("ring_next_alarm() called")
     # Make the alarm's current routine the pending routine in the global state
     state.set_current_routine(state.next_routine)
     # Pre-initialize the asynchronous play-audio function with volume and file path
