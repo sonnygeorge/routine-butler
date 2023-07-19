b9c42104c0f2e56fb31b625357b7599f69228b76
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-07-19 15:58:42 -0600

    Changed Python call to absolute path

--- rpi_startup.sh
+++ rpi_startup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # chains the necessary commands for running RoutineButler on a Raspberry Pi
 
-## Change directory to the routine-butler repository
-navigate_to_routine_butler_repository() {
-  cd /home/raspberry/routine-butler
-}
+repo_dir_path="/home/raspberry/routine-butler"
+venv_dir_path="/home/raspberry/routine-butler/venv"
+run_python_script_path="python3 run.py --single-user"
 
-if not navigate_to_routine_butler_repository; then
-  echo "Error: Failed to navigate to the /home/raspberry/routine-butler."
-  exit 1
-fi
-
-echo "Running rpi_startup.sh from the routine-butler repository directory."
 
 ## Check and activate the virtual environment
-venv_dir="/home/raspberry/routine-butler/venv"
-if [ ! -d "$venv_dir" ]; then
-  echo "Error: Virtual environment directory '$venv_dir' not found. Please create or verify the virtual environment."
+if [ ! -d "$venv_dir_path" ]; then
+  echo "Error: Virtual environment directory '$venv_dir_path' not found."
   exit 1
 fi
 
 echo "Activating virtual environment..."
 source "$venv_dir/bin/activate"
+
+## Change directory to the routine-butler repository
+navigate_to_routine_butler_repository() {
+  cd "$repo_dir_path"
+}
+
+if not navigate_to_routine_butler_repository; then
+  echo "Error: Failed to navigate to '$repo_dir_path'"
+  exit 1
+fi
 
 ## Attempt to git pull latest version
 echo "Updating with the latest version..."
@@ -44,16 +46,16 @@ fi
 
 ## Start RoutineButler
 run_routine_butler() {
-echo "Attempting to run RoutineButler in single-user mode..."
-python3 run.py --single-user
+  echo "Attempting to run RoutineButler in single-user mode..."
+  python3 "$run_python_script_path" --single-user
 }
 
 if ! run_routine_butler; then
-    echo "Error: RoutineButler failed to start. Attempting to install dependencies..."
-    pip3 install -r requirements.txt
-    echo "Re-attempting to start RoutineButler in single-user mode..."
-    if ! run_routine_butler; then
-        echo "Error: Failed to start RoutineButler even after installing dependencies."
-        exit 1
-    fi
+  echo "Error: RoutineButler failed to start. Attempting to install dependencies..."
+  pip3 install -r requirements.txt
+  echo "Re-attempting to start RoutineButler in single-user mode..."
+  if ! run_routine_butler; then
+    echo "Error: Failed to start RoutineButler even after installing dependencies."
+    exit 1
+  fi
 fi
