08e5a6fc9d48cbddaba410fddffea7475cd96649
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-07-19 14:05:33 -0600

    Enhanced rpi_startup.sh w/ error handling

--- rpi_startup.sh
+++ rpi_startup.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
 # chains the necessary commands for running RoutineButler on a Raspberry Pi
 
-# Get the current working directory's name
 current_dir=$(basename "$PWD")
-echo "Current directory: $current_dir"
+echo "Running rpi_startup.sh from current directory: '$current_dir'"
 
-echo "Running rpi_startup.sh from '$current_dir' dir"
-
-# Check if the current directory's name is "mydir"
+## Make sure the current directory's name is "routine-butler"
 if [ "$current_dir" != "routine-butler" ]; then
   echo "Error: This startup script must run from the routine_butler directory."
   exit 1
 fi
 
-echo "Updating with latest version..."
+## Check and activate the virtual environment
+venv_dir="venv"
+if [ ! -d "$venv_dir" ]; then
+  echo "Error: Virtual environment directory '$venv_dir' not found. Please create or verify the virtual environment."
+  exit 1
+fi
+
+echo "Activating virtual environment..."
+source "$venv_dir/bin/activate"
+
+## Git pull latest version
+echo "Updating with the latest version..."
+git_status=$(git status --porcelain)
+if [ -n "$git_status" ]; then
+  echo "Error: Local changes exist. Please commit or discard changes before running 'git pull'."
+  exit 1
+fi
+
 git pull
 
-echo "Setting volume..."
-amixer set PCM -- 60%
+## Set RPi volume
+# Function to set system volume
+set_system_volume() {
+  echo "Attempting to set volume..."
+  amixer set PCM -- 60%
+}
 
-echo "Starting RoutineButler in single-user mode..."
-python3 run.py --single-user
+if ! set_system_volume; then
+  echo "Error: Failed to set the volume with 'amixer set PCM -- 60%' command."
+fi
 
-echo "Opening chromium in kiosk mode..."
+## Start RoutineButler
+# Function to execute run.py
+start_routine_butler() {
+  echo "Attempting to start RoutineButler in single-user mode..."
+  python3 run.py --single-user
+}
+
+if ! start_routine_butler; then
+  echo "Error: RoutineButler failed to start. Attempting to install dependencies..."
+  pip3 install -r requirements.txt
+  echo "Re-attempting to start RoutineButler in single-user mode..."
+  if ! start_routine_butler; then
+    echo "Error: Failed to start RoutineButler even after installing dependencies."
+    exit 1
+  fi
+fi
+
+## Open Chromium in kiosk mode
+echo "Opening Chromium in kiosk mode..."
 chromium-browser --kiosk http://127.0.0.1:8080
