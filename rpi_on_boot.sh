87e0f6f1d9bb0beea3a694d82767b78635501759
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-07-20 13:24:21 -0600

    Trying new kiosk-mode options

--- rpi_on_boot.sh
+++ rpi_on_boot.sh
@@ -17,7 +17,14 @@ echo "Waiting for RoutineButler to start..."
 sleep 8
 
 echo "Starting Chromium in kiosk mode..."
-chromium-browser http://127.0.0.1:8080
 
-# Use xdotool to send F11 key to Chromium, which will trigger full-screen mode
-DISPLAY=:0 xdotool key F11
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+chromium-browser --noerrdialogs --disable-infobars --kiosk http://127.0.0.1:8080
