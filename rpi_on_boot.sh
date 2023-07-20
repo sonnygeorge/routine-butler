38bf37c82c5161950b3367c1eb1214794583ff15
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-07-20 11:53:13 -0600

    Added Chromium and environment variable setup

--- rpi_on_boot.sh
+++ rpi_on_boot.sh
@@ -5,12 +5,16 @@ REPO_DIR_PATH="/home/raspberry/routine-butler"
 VENV_DIR_PATH="/home/raspberry/routine-butler/venv"
 PYTHON_SCRIPT_PATH="/home/raspberry/routine-butler/run.py"
 
+# Set up environment variables
+export DISPLAY=:0
+export PULSE_SERVER=/run/user/1000/pulse/native
+
 ## Change directory to the routine-butler repository
 navigate_to_routine_butler_repository() {
   cd "$REPO_DIR_PATH"
 }
 
-if not navigate_to_routine_butler_repository; then
+if not navigate_to_routine_butler_repository; then  # FIXME: remove
   echo "Error: Failed to navigate to '$REPO_DIR_PATH'"
   exit 1
 fi
@@ -24,38 +28,45 @@ fi
 echo "Activating virtual environment..."
 source "$VENV_DIR_PATH/bin/activate"
 
+# ## Attempt to git pull latest version of RoutineButler
+# echo "Updating with the latest version of RoutineButler..."
+# git_status=$(git -C "$REPO_DIR_PATH" status --porcelain)
+# if [ -n "$git_status" ]; then
+#   echo "Warning: Local changes exist. Please commit or discard changes before running 'git pull'."
+# fi
 
-## Attempt to git pull latest version of RoutineButler
-echo "Updating with the latest version of RoutineButler..."
-git_status=$(git -C "$REPO_DIR_PATH" status --porcelain)
-if [ -n "$git_status" ]; then
-  echo "Warning: Local changes exist. Please commit or discard changes before running 'git pull'."
-fi
+# git -C "$REPO_DIR_PATH" pull
 
-git -C "$REPO_DIR_PATH" pull
+# ## Set RPi volume
+# set_system_volume() {
+#   echo "Attempting to set volume..."
+#   amixer set PCM -- 60%
+# }
 
-## Set RPi volume
-set_system_volume() {
-  echo "Attempting to set volume..."
-  amixer set PCM -- 60%
-}
+# if ! set_system_volume; then
+#   echo "Error: Failed to set the volume with 'amixer set PCM -- 60%' command."
+# fi
 
-if ! set_system_volume; then
-  echo "Error: Failed to set the volume with 'amixer set PCM -- 60%' command."
-fi
+echo "Attempting to run RoutineButler in single-user mode..."
+python3 "$PYTHON_SCRIPT_PATH" --single-user &
 
-## Start RoutineButler
-run_routine_butler() {
-  echo "Attempting to run RoutineButler in single-user mode..."
-  python3 "$PYTHON_SCRIPT_PATH" --single-user
-}
+sleep 5
 
-if ! run_routine_butler; then
-  echo "Error: RoutineButler failed to start. Attempting to install dependencies..."
-  pip3 install -r requirements.txt
-  echo "Re-attempting to start RoutineButler in single-user mode..."
-  if ! run_routine_butler; then
-    echo "Error: Failed to start RoutineButler even after installing dependencies."
-    exit 1
-  fi
-fi
+# Start Chromium in kiosk mode
+chromium-browser --kiosk http://127.0.0.1:8080
+
+# ## Start RoutineButler
+# run_routine_butler() {
+#   echo "Attempting to run RoutineButler in single-user mode..."
+#   python3 "$PYTHON_SCRIPT_PATH" --single-user
+# }
+
+# if ! run_routine_butler; then
+#   echo "Error: RoutineButler failed to start. Attempting to install dependencies..."
+#   pip3 install -r requirements.txt  # FIXME: absolute path
+#   echo "Re-attempting to start RoutineButler in single-user mode..."
+#   if ! run_routine_butler; then
+#     echo "Error: Failed to start RoutineButler even after installing dependencies."
+#     exit 1
+#   fi
+# fi
