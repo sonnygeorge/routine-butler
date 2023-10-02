646292a88a78ad7b2c2b73cd8f59180c77156260
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2023-10-02 01:17:47 -0400

    Fixed bug from black removing GPIO import

--- routine_butler/hardware/gpio.py
+++ routine_butler/hardware/gpio.py
@@ -1,9 +1,12 @@
 from loguru import logger
 
 try:
+    import RPi.GPIO as GPIO
 
     GPIO_IS_MOCK = False
-except Exception as e:  # noqa
+except Exception:  # noqa
+    import Mock.GPIO as GPIO
 
-    logger.warning(f"Failed to import RPi.GPIO: {e} - using Mock.GPIO instead")
     GPIO_IS_MOCK = True
+
+logger.info(f"GPIO_IS_MOCK: {GPIO_IS_MOCK} | GPIO: {GPIO.__name__}")
