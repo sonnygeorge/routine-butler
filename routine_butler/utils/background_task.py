961dea310ec868dcd1faa108cbca9925440ec675
Sonny George <56851635+sonnygeorge@users.noreply.github.com>
2024-01-03 18:06:03 -0600

    Temporary debugging measures

--- routine_butler/utils/background_task.py
+++ routine_butler/utils/background_task.py
@@ -28,12 +28,19 @@ class ScheduledBackgroundTask:
     is_pending: bool = False
 
     def should_be_run(self) -> bool:
-        """Returns True if the current day/hour matches the days/hours this task
-        is scheduled to run on.
+        """Returns True if the current day/hour matches the days/hours this task is
+        scheduled to run on.
         """
+        print(1)
         if self.is_pending:
             return True
         else:
+            print(
+                print(datetime.datetime.now().weekday()),
+                "in",
+                self.days_to_run,
+            )
+            print(datetime.datetime.now().hour, "in", self.hours_to_run)
             return (
                 datetime.datetime.now().weekday() in self.days_to_run
                 and datetime.datetime.now().hour in self.hours_to_run
@@ -52,75 +59,68 @@ class HourlyBackgroundTaskManager:
         This method is intended to be run on an interval <= 1 hour.
         """
         log_this("Checking if new hour has arrived...")
-        if datetime.datetime.now().hour != self.last_hour_run:
+        if True:  # FIXME: datetime.datetime.now().hour != self.last_hour_run:
             await self.hourly_run_tasks()
             self.last_hour_run = datetime.datetime.now().hour
         else:
-            logger.log(BG_TASK_LOG_LVL, "New hour has not arrived.")
+            log_this("New hour has not arrived.")
 
     async def hourly_run_tasks(self):
         """Intended to be run once per hour. Checks if any tasks are scheduled to run
         on the current day/hour. If so, asserts their preconditions and runs the
         task(s).
         """
-        log_this(
-            "New hour arrived, checking for tasks to run...",
-        )
+        log_this("New hour arrived, checking for tasks to run...")
         task_was_found_to_run = False
         for task in self._tasks:
-            if task.should_be_run():
-                task_was_found_to_run = True
-                failed_checks = False
-                for check in task.pre_checks_that_must_be_true:
+            print(0)
+            if not task.should_be_run():
+                continue
+            print(2)
+            task_was_found_to_run = True
+            failed_checks = False
+            task_name = task.func.__name__
+            print(3)
+            log_this(f"Found '{task_name}'. Asserting preconditions...")
+            for check in task.pre_checks_that_must_be_true:
+                if asyncio.iscoroutinefunction(check):
+                    was_true = await check()
+                else:
+                    was_true = check()
+                if not was_true:
                     log_this(
-                        f"Found '{task.func.__name__}' to run. Asserting "
-                        "preconditions are true..."
+                        f"Precondition '{check.__name__}' failed for task "
+                        f"'{task_name}'. Skipping task.",
                     )
-                    if asyncio.iscoroutinefunction(check):
-                        was_true = await check()
-                    else:
-                        was_true = check()
-                    if not was_true:
-                        log_this(
-                            f"Precondition '{check.__name__}' failed for task "
-                            f"'{task.func.__name__}'. Skipping task.",
-                        )
-                        failed_checks = True
-                        break
-                if not failed_checks:
-                    if asyncio.iscoroutinefunction(task.func):
-                        was_success = await task.func()
-                    elif task.is_io_bound:
-                        was_success = await run.io_bound(task.func)
-                    else:
-                        was_success = await run.cpu_bound(task.func)
-                    if was_success:
-                        log_this(
-                            f"Task '{task.func.__name__}' completed "
-                            "successfully.",
-                        )
-                        task.is_pending = False
-                    else:
-                        log_this(
-                            f"Task '{task.func.__name__}' failed. Will retry "
-                            "next hour.",
-                        )
-                        task.is_pending = True
+                    failed_checks = True
+                    break
+            if not failed_checks:
+                if asyncio.iscoroutinefunction(task.func):
+                    was_success = await task.func()
+                elif task.is_io_bound:
+                    was_success = await run.io_bound(task.func)
+                else:
+                    was_success = await run.cpu_bound(task.func)
+                if was_success:
+                    log_this(f"Task '{task_name}' completed successfully.")
+                    task.is_pending = False
+                else:
+                    log_this(f"Task '{task_name}' failed. Marked pending.")
+                    task.is_pending = True
         if not task_was_found_to_run:
-            log_this(
-                "No tasks were found to be run.",
-            )
+            log_this("No tasks were found to be run at this time.")
 
 
 async def perform_db_backup() -> bool:
     """Uploads a copy of the DB to the cloud storage bucket."""
     try:
         await STORAGE_BUCKET.upload(
-            local_path=DB_PATH, remote_dir_path=f"{DB_BACKUP_FOLDER_NAME}/"
+            local_path=DB_PATH,
+            remote_dir_path=f"{DB_BACKUP_FOLDER_NAME}/",
         )
         return True
     except Exception as e:
-        logger.warning(f"DB backup failed with error: {e}")
+        logger.warning(f"'perform_db_backup' failed with error: {e}")
         return False
 
 
